0ad2022eff21746ed663c7b590d0e8eeafb1a9b8
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-10-07 20:17:58 -0500

    start screen established

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ const game = ()=> {
     let pScore = 0 ;
     let cScore = 0 ;
 
+    //This function below starts the game or has the fade transitions
     const startGame = () =>{
         const playBtn = document.querySelector('.intro button') ;
-    }    
-}
+        const introScreen = document.querySelector('.intro') ;
+        const match = document.querySelector('.match') ;
+
+        playBtn.addEventListener('click', () =>{
+            introScreen.classList.add('fadeOut') ;
+            match.classList.add('fadeIn') ;
+        }) ;
+    } ; 
+    //call out all inner functions
+    startGame() ; 
+} ;
+
+game() ;
