3f0210234f7c36cc728f083feb997811d2d07455
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-10-07 21:28:36 -0500

    complete js
    player image disappears & buttons clutter

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ const game = ()=> {
         const options = document.querySelectorAll('.options button') ;
         const playerHand = document.querySelector('.player-hand') ;
         const computerHand = document.querySelector('.computer-hand') ;
+        const hands = document.querySelectorAll('.hands img') ;
+
+        hands.forEach(hand =>{
+            hand.addEventListener('animationend', function(){
+                this.style.animation = "" ;
+            }) ;
+        })
         //computer options
         const computerOptions = ['rock', 'paper', 'scissors'] ;
 
@@ -26,12 +33,18 @@ const game = ()=> {
             //computer generated choice
             const computerNumber = Math.floor(Math.random() * 3) ; 
             const computerChoice = computerOptions[computerNumber] ;
+
+            setTimeout(() => {
             //This is where we call compare hands
             compareHands(this.textContent, computerChoice) ;
 
             //Update Images
             playerHand.src = `./assets/${this.textContent}.png` ;
             computerHand.src = `./assets/${computerChoice}.png` ;
+            }, 2000) ;
+            //Hand Animation
+            playerHand.style.animation = "shakePlayer 2s ease" ;
+            computerHand.style.animation = "shakeComputer 2s ease" ;
             }) ;
         }) ;
 
