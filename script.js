dddd8459e47cbf40e05538d118a4cd01066f87e1
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-10-08 10:27:11 -0500

    start game screen

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+const game = () => {
+    let pScore = 0 ;
+    let cScore = 0 ;
 
+    //Start Game Screen
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button') ;
+        const introScreen = document.querySelector('.intro') ;
+        const match = document.querySelector('.match') ;
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut') ;
+            match.classList.add('fadeIn') ;
+        }) ;
+    } ;
+}
 
